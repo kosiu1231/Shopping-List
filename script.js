f3e61836229147b9d889f937b5981084e684921c
kosiu1231 <aroczek1@wp.pl>
2023-03-20 14:10:07 +0100

    Added local storage, add item and delete all buttons.

--- script.js
+++ script.js
@@ -2,20 +2,80 @@ const addItemDiv = document.querySelector("#addNewParagraph");
 let elem = "";
 let node = "";
 
-function addParagraph(name) {
-    elem = document.createElement("p");
-    node = document.createTextNode(`- ${name}`);
-    elem.appendChild(node);
-    elem.classList.add("text-3xl", "sm:text-4xl");
+const getItems = () => JSON.parse(localStorage.getItem("items")) || []; //returns null, make empty arr instead
+const saveItems = (items) => localStorage.setItem("items", JSON.stringify(items));
 
-    addItemDiv.parentNode.insertBefore(elem, addItemDiv);
+function addParagraph(name) {
+    const existingParagraphs = document.querySelectorAll("p");
+    const existingNames = Array.from(existingParagraphs).map(p => p.textContent.substring(2));
+
+    if (!existingNames.includes(name)) {
+        elem = document.createElement("p");
+        node = document.createTextNode(`- ${name}`);
+        elem.appendChild(node);
+        elem.classList.add("text-3xl", "sm:text-4xl");
+
+        addItemDiv.parentNode.insertBefore(elem, addItemDiv);
+
+        const items = getItems();
+        items.push(name);
+        saveItems(items);
+    }
+}
+
+const removeAllItems = () => localStorage.removeItem("items");
+
+const removeAllParagraphs = () => {
+    const items = getItems();
+    removeAllItems();
+
+    const paragraphs = document.querySelectorAll("p.text-3xl");
+
+    for (let i = 0; i < paragraphs.length - 1; i++) {
+        paragraphs[i].remove();
+      }
+};
+
+function addItem() {
+    addParagraph(newItemTextbox.value);
+    newItemTextbox.value = "";
 }
 
 const newItemTextbox = document.querySelector("#newItemTextbox");
 
 newItemTextbox.addEventListener("keydown", function (e) {
-    if (e.code === "Enter" && newItemTextbox.value != "") {
-        addParagraph(newItemTextbox.value);
-        newItemTextbox.value = "";
+    if ((e.code === "Enter" || e.code === 13) && newItemTextbox.value != "") {
+        addItem();
     }
 });
+
+const addIcon = document.querySelector(".gg-math-plus");
+
+addIcon.addEventListener("click", function (e) {
+    addItem();
+});
+
+const deleteIcon = document.querySelector(".fa");
+
+deleteIcon.addEventListener("click", function (e) {
+    removeAllParagraphs();
+});
+
+// const removeParagraph = (index) => {
+//     const items = getItems();
+//     items.splice(index, 1);
+//     saveItems(items);
+// };
+
+// const updateItem = (index, value) => {
+//     const items = getItems();
+//     items[index] = value;
+//     saveItems(items);
+// };
+
+const init = () => {
+    const items = getItems();
+    items.forEach((item) => addParagraph(item));
+  };
+  
+  init();
